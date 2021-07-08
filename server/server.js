92390ee6ee14445f77cd7c899166664e1e1d85b5
@maksim.ivanovskij <maximilianepc@outlook.com>
2021-07-08 18:10:45 +0300

    MongoDB connection.

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const MongoClient = require('mongodb').MongoClient;
 const fs = require('fs');
 const dateFormat = require('dateformat');
 const app = express();
@@ -8,25 +9,43 @@ const port = 8080;
 app.use(express.json());
 app.use(cors());
 
-app.post('/logOpen', (req, res) => {
-    const logMessage = `[${dateFormat(new Date(), 'yyyy-mm-dd HH:MM:ss')}] ${req.body.source} article '${req.body.title}' at ${req.body.url} opened.`;
-    if(!fs.existsSync('logs')) {
-        fs.mkdirSync('logs');
-    }
-    fs.writeFile(`logs/${dateFormat(new Date(), 'yyyymmdd')}_log.log`, `${logMessage}\n`, { flag: 'a' }, err => {});
-    console.log(logMessage);
-    res.status(204).send();
-})
+MongoClient.connect('mongodb://localhost:27017/newsLogs', { useUnifiedTopology: true })
+    .then(client => {
+        console.log('Connected to database.')
+        const db = client.db('newsLogs');
+        
+        app.post('/logOpen', (req, res) => {
+            const dateTime = new Date()
+            const logMessage = `[${dateFormat(dateTime, 'yyyy-mm-dd HH:MM:ss')}] ${req.body.source} article '${req.body.title}' at ${req.body.url} opened.`;
+            if(!fs.existsSync('logs')) {
+                fs.mkdirSync('logs');
+            }
+            fs.writeFile(`logs/${dateFormat(new Date(), 'yyyymmdd')}_log.log`, `${logMessage}\n`, { flag: 'a' }, err => {});
+            console.log(logMessage);
+            db.collection('opens').insertOne({
+                dateTime: dateTime.toString(),
+                ...req.body,
+            })
+            res.status(204).send();
+        })
 
-app.post('/logSearch', (req, res) => {
-    const logMessage = `[${dateFormat(new Date(), 'yyyy-mm-dd HH:MM:ss')}] '${req.body.query}' was searched.`;
-    if(!fs.existsSync('logs')) {
-        fs.mkdirSync('logs');
-    }
-    fs.writeFile(`logs/${dateFormat(new Date(), 'yyyymmdd')}_log.log`, `${logMessage}\n`, { flag: 'a' }, err => {});
-    console.log(logMessage);
-    res.status(204).send();
-})
+        app.post('/logSearch', (req, res) => {
+            const dateTime = new Date()
+            const logMessage = `[${dateFormat(dateTime, 'yyyy-mm-dd HH:MM:ss')}] '${req.body.query}' was searched.`;
+            if(!fs.existsSync('logs')) {
+                fs.mkdirSync('logs');
+            }
+            fs.writeFile(`logs/${dateFormat(new Date(), 'yyyymmdd')}_log.log`, `${logMessage}\n`, { flag: 'a' }, err => {});
+            console.log(logMessage);
+            db.collection('searches').insertOne({
+                dateTime: dateTime.toString(),
+                ...req.body,
+            })
+            res.status(204).send();
+        })
+
+    })
+    .catch(error => console.error(error));
 
 app.listen(port, () => {
     console.log(`Server listening at port ${port}.`);
